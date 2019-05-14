02a6f6dc2ad2a314e129809ffc3b5aa57d7c343c
Lukoszko <36648877+Lukoszko@users.noreply.github.com>
2019-05-14 21:29:40 +0200

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int Out(int a)
+{
+    for (int i=0;i<1;i++) {
+    cout << a;
+    }
+    
+    return 0;
+}
+int main()
+{
+    
+    int in;
+    for (int i=0;i<1;i++) {
+    cin >> in;
+}
+    
+    
+    Out(in); 
+    
+    return 0;
+}
